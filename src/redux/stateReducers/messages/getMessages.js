80cb5e5947d746ad5d130279619a569ff1e795f6
AlvaradoM <mauriciomj11@gmail.com>
2019-12-12 06:35:36 -0800

    infinite scroll and likesbug

--- src/redux/stateReducers/messages/getMessages.js
+++ src/redux/stateReducers/messages/getMessages.js
@@ -1,4 +1,4 @@
-import { GETMESSAGES } from "../../actionTypes";
+import { GETMESSAGES,GETMESSAGE,GETMOREMESSAGES } from "../../actionTypes";
 import { withAsyncReducer } from "../../HORs";
 
 const initialState = {
@@ -9,9 +9,60 @@ const initialState = {
 
 const getMessages = (state = initialState, action) => {
   switch (action.type) {
+    case GETMESSAGE.START:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case GETMESSAGE.SUCCESS:
+      const newMessages = state.result.messages.slice();
+      const messageIndex = newMessages.findIndex(
+        message => message.id === action.payload.message.id
+      );
+      newMessages.splice(messageIndex, 1, action.payload.message);
+      return {
+        ...state,
+        loading: false,
+        result: {
+          ...state.result,
+          messages: newMessages
+        }
+      };
+    case GETMESSAGE.FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
+    case GETMOREMESSAGES.START:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case GETMOREMESSAGES.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        result: {
+          ...state.result,
+          ...action.payload,
+          messages: [...state.result.messages, ...action.payload.messages]
+        }
+      };
+    case GETMOREMESSAGES.FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
 };
+
 
 export default withAsyncReducer(GETMESSAGES, getMessages);
